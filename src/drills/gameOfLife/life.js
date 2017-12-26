b1cef4d8ac7fbfe7cec68dc505083527fcc4c025
Maxwell Russell <maxruss.87@gmail.com>
2017-12-26 16:43:48 -0600

    Update life.js
    Adding in piping for functions as well as updater that returns new generation objects.

--- src/drills/gameOfLife/life.js
+++ src/drills/gameOfLife/life.js
@@ -1,9 +1,21 @@
+const _pipe = (a, b) => (...args) => b(a(...args));
+const pipe = (...fns) => fns.reduce(_pipe);
+
 function getNextGeneration(cells, generations) {
-  // store the computed current generation
-  const currentGen = generateGenState(cells)
-  const nextGen = generateNextGen(currentGen);
+  const initiateSeed = pipe(generateGenState, generateNextGenState, setNextGenState);
+  const progressLife = pipe(generateNextGenState, setNextGenState)
+
   const noCells = cells.length === 0 ? true : false;
-  return noCells ? [] : nextGen;
+  const areSeedCells = Array.isArray(cells) === true ? true : false;
+  const live = generations > 0 ? true : false
+
+  return noCells
+    ? []
+    : live
+      ? areSeedCells
+        ? getNextGeneration(initiateSeed(cells), generations - 1)
+        : getNextGeneration(progressLife(cells), generations - 1)
+      : 'last gen'
 }
 
 function generateGenState(rawCells) {
@@ -13,18 +25,29 @@ function generateGenState(rawCells) {
         const toroidalLimits = [arr.length, arr2.length]
         const cellState = cell;
         const cellObj = createCell(i + 1, p + 1, cellState, toroidalLimits);
-        return genState[cellObj.cellHash] === undefined ? genState[cellObj.cellHash] = cellObj : null
+        return genState[cellObj.cellHash] = cellObj
       });
    });
   return genState;
 }
 
-function generateNextGen(gen) {
+function generateNextGenState(gen) {
   // for each Cell in our hashmap see if the value changes
   for (let cell in gen) {
     gen[cell] = getNextCellState(gen[cell], gen)
   }
   return gen;
+}
+
+function setNextGenState(gen) {
+  let newGen = {};
+  for (let cell in gen) {
+    newGen[cell] = changeCellState(gen[cell])
+  }
+  console.log(gen);
+  console.log(newGen);
+  // because of the way this factory works, in order to be able to actively track the object changes without implementing some sort of timeout/promise setup we need to return a copy of our object at each change pass. I anticipate needing to store some sort of emit
+  return newGen;
 }
 
 function createCell(x, y, cellState, toroidalLimits) {
@@ -38,29 +61,19 @@ function createCell(x, y, cellState, toroidalLimits) {
     cellState,
     toroidalLimits
   });
-
   return Cell();
 }
 
 function getNextCellState(cell, genState) {
-  // if the x value is 1 we need to check against x values with toroidalLimits[0]
-  // if the x value is 5 we need to check against x values of 1
-  // if the y value is 1 we need to check against y values toroidalLimits[1]
-  // if the y value is 5 we need to check against y values of 1
   const lowLimit = cell.x === cell.toroidalLimits[0] ? 1 : cell.x + 1;
   const highLimit = cell.x === 1 ? cell.toroidalLimits[0] : cell.x - 1;
   const rightLimit = cell.y === cell.toroidalLimits[1] ? 1 : cell.y + 1;
   const leftLimit = cell.y === 1 ? cell.toroidalLimits[1] : cell.y - 1;
 
   const blockSum = [
-      [highLimit, leftLimit],
-      [highLimit, cell.y],
-      [highLimit, rightLimit],
-      [cell.x, leftLimit], /* current cell */
-      [cell.x, rightLimit],
-      [lowLimit, leftLimit],
-      [lowLimit, cell.y],
-      [lowLimit, rightLimit]
+      [highLimit, leftLimit], [highLimit, cell.y], [highLimit, rightLimit],
+      [cell.x, leftLimit], [cell.x, cell.y], [cell.x, rightLimit],
+      [lowLimit, leftLimit], [lowLimit, cell.y], [lowLimit, rightLimit]
       ].map(neighbor => {
         return genState[(neighbor[0] * 15486047) + (neighbor[1] * 15487429)].cellState;
       }).reduce((a, b) => a + b);
@@ -69,7 +82,13 @@ function getNextCellState(cell, genState) {
   const sum4 = blockSum === 4 ? true : false
   const nextState = sum3 ? 1 : sum4 ? cell.cellState : 0;
 
-  return {...cell, nextState };
+  return { ...cell, nextState };
 }
 
+function changeCellState(cell) {
+  const nextState = cell.nextState;
+  const cellState = nextState;
+  // this is what will need to change the visuals if I put this on screen
+  return { ...cell, cellState, nextState: null}
+}
 export { getNextGeneration, generateGenState, generateNextGen, createCell, getNextCellState };
