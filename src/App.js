9c27447b7d346f4a6fb2c4a942a425cd41774c92
Maxwell Russell <maxruss.87@gmail.com>
2017-12-27 14:16:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
+import Routes from './Routes';
+import RouteNavItem from './components/RouteNavItem';
+
 import './App.css';
 
 import { getNextGeneration } from './drills/gameOfLife/life';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
-    this.logTest();
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <h1 className="App-title">Practice Hub</h1>
         </header>
-        <p className="App-intro">
-          A hub to practice and version control JavaScript Drills
-        </p>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Practice Hub</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {
+              [
+              <RouteNavItem key={ 1 } href="/gameOfLife">
+                Game of Life
+              </RouteNavItem>
+              ]
+            }
+          </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={ childProps } />
       </div>
     );
-  }
-
-  logTest() {
-    let gen = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0]
-    ];
-
-    console.log(getNextGeneration(gen, 1));
   }
 }
 
