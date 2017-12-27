c5041d5a4cea9634e73934ba90b9dd592da4f954
Maxwell Russell <maxruss.87@gmail.com>
2017-12-27 16:30:49 -0600

    Update lifeBoard.js

--- src/containers/gameOfLife/lifeBoard.js
+++ src/containers/gameOfLife/lifeBoard.js
@@ -11,14 +11,28 @@ export default class GameOfLife extends Component {
     lifeBound: [5, 5],
     cells: []
   }
+
+  componentWillMount() {
+    store.seed = 
+  }
   
+  componentWillUnmount() {
+    store.seed = [];
+    store.generation = {};
+  }
+    
   render() {
-    let cells = Array.from(new Array(10), () => <div>cell</div>);
+    let cells = Array.from(new Array(10), (i) => <div key={ i } onClick={ this.handleSelect }>cell</div>);
     return (
       <div className="LifeBoard">
         A board and related settings bars will be rendered here.
         { cells }
       </div>
     );
+  }
+    
+  handleSelect(e) {
+    
+    
   }
 }
