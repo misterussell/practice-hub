63db0625b8b251917c71aa6e6aa8a5e9ff74c961
Maxwell Russell <maxruss.87@gmail.com>
2017-12-27 15:33:34 -0600

    Update lifeBoard.js

--- src/containers/gameOfLife/lifeBoard.js
+++ src/containers/gameOfLife/lifeBoard.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
+import Store from './store';
+
 export default class GameOfLife extends Component {
   constructor(props) {
     super(props);
   }
   
+  this.state = {
+    lifeBound: [5, 5],
+    cells: []
+  }
+  
   render() {
+    let cells = Array.from(new Array(10), () => <div>cell</div>);
     return (
       <div className="LifeBoard">
         A board and related settings bars will be rendered here.
+        { cells }
       </div>
     );
   }
