8a82c7625940d620eb639e7b9747461b906fc753
misterussell <max@misterussell.com>
2018-11-08 13:09:22 -0600

    adding initial files for csv converter

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,8 @@
 import Cells from './collections/Cells';
 import Tracking from './collections/LifeTracking';
 
+import MobXCell from './models/mobXCell';
+
 let store = {
   cells: Cells(),
   tracking: Tracking(),
---
+++ src/models/mobXCell.js
@@ -0,0 +1,7 @@
+export default function test() {
+
+  return Object.freeze({
+    test: true
+  });
+
+}
